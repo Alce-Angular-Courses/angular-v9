93f457f2bb7313617d08f60cee96a5f34e1aa292
alce65 <alce65@hotmail.es>
2020-06-01 20:27:19 +0200

    App inicial

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-v9 app is running!');
+    expect(page.getTitleText()).toEqual('Angular 9 app is running!');
   });
 
   afterEach(async () => {
